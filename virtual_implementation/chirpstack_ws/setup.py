12b282527c6d24fb73c3afbddbd940a9111d39a2
Filippo <f.barbieri4@studenti.unibg.it>
2023-02-02 15:49:34 +0100

    update app_id and api_token

--- virtual_implementation/chirpstack_ws/setup.py
+++ virtual_implementation/chirpstack_ws/setup.py
@@ -4,7 +4,7 @@ port = 1883
 ip = broker_server + ":8080"
 broker_gw = broker_server 
 
-application_id = "fda23245-f5e7-411f-8d9f-d1cac850c286"
-api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6ImY1ODk2NjYzLWQzZmUtNGYwMi04MTc1LWQ3OGYzNzM1NDJmMSIsInR5cCI6ImtleSJ9.oyxsjle9R0AyQ57_CYeqoMaoCTs2bvGatpxD7DzDToo"
+application_id = "4470df86-c1b0-4cb0-9f12-2100328d3fea"
+api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6Ijg5ODM5NmE4LTM5MzMtNDA0Zi1hMDlmLWEwNDhhYjUzYWRlMSIsInR5cCI6ImtleSJ9.XA4krnmtj56Jvr18kJl8dg1-LwX4Pvy6BEgZESAzVMs"
 
 
